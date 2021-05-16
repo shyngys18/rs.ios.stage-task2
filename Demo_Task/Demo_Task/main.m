2e2b9a98a50e44c9342ce34acf3b6c11362af97c
Shyngys <Shyngys>
2021-05-16 22:51:03 +0600

    f

---
+++ Demo_Task/Demo_Task/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo_Task
+//
+//  Created by Шынгыс on 4/29/21.
+//  Copyright © 2021 Шынгыс. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
